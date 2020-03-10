229ba0488e91c4b113aac62f4691b51be05013e6
Jayajit <jayajit1989@gmail.com>
2020-03-10 11:48:44 +0530

    BaseMvpKotlin

--- app/src/main/java/com/example/basemvpkotlin/ui/activity/MainActivityRepository.kt
+++ app/src/main/java/com/example/basemvpkotlin/ui/activity/MainActivityRepository.kt
@@ -2,11 +2,9 @@ package com.example.basemvpkotlin.ui.activity
 
 class MainActivityRepository : MainActivityMVP.IRepository {
 
-    private var mainActivityPresenter: MainActivityPresenter<MainActivityMVP.IView>? = null
 
-    init {
-        mainActivityPresenter = MainActivityPresenter()
-    }
 
     /*Rest call method from presenter*/
+
+
 }
